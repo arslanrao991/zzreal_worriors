fbd7de26ba232e7c970657a421bd9345a4676f36
YousafQadri <90567366+YousafQadri@users.noreply.github.com>
2023-03-20 01:52:04 +0500

    Yousaf

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,28 +1,16 @@
-const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
-// turn on headless mode when running with HEADLESS=true environment variable
-// export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
-
-// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
-setCommonPlugins();
-
 /** @type {CodeceptJS.MainConfig} */
-exports.config = 
-{
+exports.config = {
   tests: './*_test.js',
   output: './output',
-  helpers: 
-  {
-    Puppeteer: 
-    {
+  helpers: {
+    Puppeteer: {
       url: 'http://localhost:3000/',
       show: true,
       windowSize: '1200x900'
     }
   },
-  include: 
-  {
+  include: {
     I: './steps_file.js'
   },
-  name: 'zzreal_worriors'
+  name: 'STFinal'
 }
